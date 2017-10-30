ae68dd76df66eee887939f329405ef045ea174f6
kaur <kaur.palang@hotmail.com>
2017-10-30 14:40:36 +0200

    Getting a working source to cloud

---
+++ source/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace FacebookMessenger
+{
+	partial class BotMain
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.WB_MainWindow = new System.Windows.Forms.WebBrowser();
+			this.Timer_ScriptInsert = new System.Windows.Forms.Timer(this.components);
+			this.Timer_Utility = new System.Windows.Forms.Timer(this.components);
+			this.SuspendLayout();
+			// 
+			// WB_MainWindow
+			// 
+			this.WB_MainWindow.AllowWebBrowserDrop = false;
+			this.WB_MainWindow.Location = new System.Drawing.Point(12, 12);
+			this.WB_MainWindow.MinimumSize = new System.Drawing.Size(20, 20);
+			this.WB_MainWindow.Name = "WB_MainWindow";
+			this.WB_MainWindow.ScrollBarsEnabled = false;
+			this.WB_MainWindow.Size = new System.Drawing.Size(1063, 450);
+			this.WB_MainWindow.TabIndex = 0;
+			this.WB_MainWindow.WebBrowserShortcutsEnabled = false;
+			// 
+			// Timer_ScriptInsert
+			// 
+			this.Timer_ScriptInsert.Interval = 1000;
+			this.Timer_ScriptInsert.Tick += new System.EventHandler(this.ResetTimer_Tick);
+			// 
+			// Timer_Utility
+			// 
+			this.Timer_Utility.Enabled = true;
+			this.Timer_Utility.Interval = 2000;
+			this.Timer_Utility.Tick += new System.EventHandler(this.Timer_Utility_Tick);
+			// 
+			// BotMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1087, 474);
+			this.Controls.Add(this.WB_MainWindow);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.Name = "BotMain";
+			this.Text = "Facebook Messenger Groupchat Bot";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.WebBrowser WB_MainWindow;
+		private System.Windows.Forms.Timer Timer_Utility;
+		private System.Windows.Forms.Timer Timer_ScriptInsert;
+	}
+}
+
